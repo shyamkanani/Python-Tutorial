aa881eb1a0d11a2f94afd915fd249f0c3e1afbb5
Harsh Mandaliya <hashmand2@gmail.com>
2018-10-09 15:50:06 +0530

    While It's while loop.....

---
+++ LoopsInPython/WhileLoop/WhileLoop.py
@@ -0,0 +1,16 @@
+'''While Loop
+while Loop is used to execute number of statements or body till the condition passed in while is true. Once the condition is false, the control will come out of the loop.
+
+Syntax:
+
+while <expression>:  
+        Body  
+Here, body will execute multiple times till the expression passed is true. The Body may be a single statement or multiple statement.
+
+Eg:'''
+
+a=10  
+while a>0:  
+    print "Value of a is",a  
+    a=a-2  
+print "Loop is Completed"
