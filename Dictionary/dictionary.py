8dc130ca812ac5e1e63596b76e2c1666c402b7c6
Logan <36381621+LoganJM@users.noreply.github.com>
2018-10-09 15:24:41 -0400

    Dictionary.py grammar and pep8 formatting. Adding a .gitignore for easier Git work

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,47 +1,53 @@
-#Author: Luís Duarte
-#Date: 07/10/2018
+# Author: Luís Duarte, LoganJM
+# Date: 07/10/2018
 
 """
-Dictionary is an unordered, they are dynamic, mutable (you can change them).
+Dictionaries are unordered, dynamic, and mutable (you can change them).
 
 They are different than lists because:
-    - Lists are accessed by their position like 0, 1, 2, 3, but dictionaries are accessed by a key"""
+    - Lists are accessed by their position (like 0, 1, 2, 3), but dictionaries
+      are accessed by a key of any value
+"""
 
-#We can define a dictionary by a pair of curly brackets
+# We can define a dictionary by a pair of curly brackets
 
 dictionary = {}
 
-#To add a value to a dictionary we need a key and a value seperated by a colon (:)
+# To add a value to a dictionary we need a key and a value seperated by a colon
 
 dictionary = {
     'key': "value"
 }
 
-#If we want to access we can retrive the value from the dictionary using a pair of square brackets [] and the key
+# If we want to access we can retrive the value from the dictionary using a
+# pair of square brackets [] and the key
 
-print(dictionary['key']) # It will output 'value'
+print(dictionary['key'])  # It will output 'value'
 
-#To add more than one key to a dictionary we need seperate them by a comma (,)
+# To add more than one key to a dictionary we need seperate them by a comma (,)
 dictionary = {
     'car': "Ford",
     'year': 2018
-
 }
 
-#If we need to change a value in a key we can:
+# If we need to change a value in a key we can:
 
 dictionary['car'] = "BMW"
-print(dictionary['car']) #Instead outputting 'Ford' it will output 'BMW'
+print(dictionary['car'])  # Instead outputting 'Ford' it will output 'BMW'
 
-#Because dictionaries are dynamic, we can add more values without needing to create a new dictionary
+# Because dictionaries are dynamic, we can add more values without needing to
+# create a new dictionary
 
 dictionary['Model'] = "i3"
-print(dictionary) #It will output {'car': 'Ford', 'year': 2018, 'Model':'i3'} It printed the new key and value  that we defined
+print(dictionary)
+# It will output {'car': 'Ford', 'year': 2018, 'Model':'i3'} It printed the new
+# key and value  that we defined
 
 
-#To remove a single value:
+# To remove a single value:
 del dictionary['car']
-print(dictionary) # It will output {'year': 2018, 'Model':'i3'}
-#To Remove the WHOLE dictionary is simple:
+print(dictionary)  # This will output {'year': 2018, 'Model':'i3'}
+
+# To Remove the WHOLE dictionary is simple:
 del dictionary
-print(dictionary) # It will output ValueError: 'dictionary' is not defined.
+print(dictionary)  # This will output ValueError: 'dictionary' is not defined.
