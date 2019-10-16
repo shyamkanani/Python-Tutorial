5e87a17a823c779906f24700d4590c3eb2a3878e
Hetal Mangukia <hmangukia9@gmail.com>
2019-10-16 11:24:08 +0530

    Update Comments.py

--- Comments/Comments.py
+++ Comments/Comments.py
@@ -3,7 +3,8 @@ Comments are lines beginning with #.
 You can write whatever you want after the # and Python will ignore it.
 Comments can make your code easier for other people to understand.
 """
-#You can also write multiline comment via writing in between '"""'called Docstring.
+#You can also write multiline comment via writing in between '"""' called Docstring.
+# Docstrings (or documentation strings) can be accessed by function_name.__doc__
 
 #(Comment) Printing Hello World
 print("Hello World!!")
